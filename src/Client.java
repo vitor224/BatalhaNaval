c07124d1f48bfb63e37a2f5be3c7fb86fe245b52
vitor224 <vitordonascimento714@gmail.com>
2023-07-03 20:12:47 -0300

    BatalhaNaval

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o endereço IP do adversário: ");
+        String ipAddress = scanner.nextLine();
+
+        // Agora você pode usar a variável 'ipAddress' para estabelecer a conexão com o adversário
+        // e iniciar o jogo
+
+        // Exemplo de uso:
+        // Game game = new Game(ipAddress);
+        // game.start();
+
+        scanner.close();
     }
 }
